7b11382b0117cfc8173e102751214ea5de74cf80
splitCoding <voicesplit@daum.net>
2023-12-23 10:23:14 +0900

    test: TestMemberTokenGenerator fixture 작성

---
+++ src/test/java/com/poolex/poolex/support/TestMemberTokenGenerator.java
@@ -0,0 +1,31 @@
+package com.poolex.poolex.support;
+
+import com.poolex.poolex.login.domain.Member;
+import com.poolex.poolex.login.domain.MemberNickname;
+import com.poolex.poolex.login.domain.MemberRepository;
+import com.poolex.poolex.token.JwtTokenProvider;
+
+public class TestMemberTokenGenerator {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public TestMemberTokenGenerator(final MemberRepository memberRepository, final JwtTokenProvider jwtTokenProvider) {
+        this.memberRepository = memberRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public String createTokenWithNewMember(final String nickname) {
+        final Member member = createMember(nickname);
+        return createAccessToken(member);
+    }
+
+    private Member createMember(final String nickname) {
+        final Member member = Member.withoutId(new MemberNickname(nickname));
+        return memberRepository.save(member);
+    }
+
+    private String createAccessToken(final Member member) {
+        return jwtTokenProvider.createAccessToken(member.getId());
+    }
+}
