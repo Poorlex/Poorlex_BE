b219170aee6c02651a00b8bb5b3c8157870caff9
splitCoding <voicesplit@daum.net>
2023-12-23 10:21:40 +0900

    test: 배틀 참가 Controller, Service 테스트 작성

---
+++ src/test/java/com/poolex/poolex/participate/controller/BattleParticipantControllerTest.java
@@ -0,0 +1,63 @@
+package com.poolex.poolex.participate.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.poolex.poolex.battle.fixture.BattleCreateRequestFixture;
+import com.poolex.poolex.battle.service.BattleService;
+import com.poolex.poolex.login.domain.Member;
+import com.poolex.poolex.login.domain.MemberNickname;
+import com.poolex.poolex.login.domain.MemberRepository;
+import com.poolex.poolex.support.IntegrationTest;
+import com.poolex.poolex.support.ReplaceUnderScoreTest;
+import com.poolex.poolex.token.JwtTokenProvider;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+class BattleParticipantControllerTest extends IntegrationTest implements ReplaceUnderScoreTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BattleService battleService;
+
+    @Test
+    void 배틀참가자를_추가한다() throws Exception {
+        //given
+        final Member member = createMember();
+        final Long battleId = createBattle(member.getId());
+        final String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+
+        //when
+        //then
+        mockMvc.perform(
+                post("/battles/{battleId}/participants", battleId)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+            )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(header().exists(HttpHeaders.LOCATION));
+    }
+
+    private Member createMember() {
+        final Member member = Member.withoutId(new MemberNickname("nickname"));
+        return memberRepository.save(member);
+    }
+
+    private Long createBattle(final Long memberId) {
+        return battleService.create(memberId, BattleCreateRequestFixture.simple());
+    }
+}
---
+++ src/test/java/com/poolex/poolex/participate/service/BattleParticipantServiceTest.java
@@ -0,0 +1,111 @@
+package com.poolex.poolex.participate.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.poolex.poolex.battle.domain.Battle;
+import com.poolex.poolex.battle.domain.BattleParticipantSize;
+import com.poolex.poolex.battle.domain.BattleRepository;
+import com.poolex.poolex.battle.domain.BattleStatus;
+import com.poolex.poolex.battle.fixture.BattleFixture;
+import com.poolex.poolex.login.domain.Member;
+import com.poolex.poolex.login.domain.MemberNickname;
+import com.poolex.poolex.login.domain.MemberRepository;
+import com.poolex.poolex.participate.domain.BattleParticipant;
+import com.poolex.poolex.participate.domain.BattleParticipantRepository;
+import com.poolex.poolex.support.ReplaceUnderScoreTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class BattleParticipantServiceTest implements ReplaceUnderScoreTest {
+
+    @Autowired
+    private BattleRepository battleRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BattleParticipantRepository battleParticipantRepository;
+    private BattleParticipantService battleParticipantService;
+
+    @BeforeEach
+    void setUp() {
+        battleParticipantService = new BattleParticipantService(battleRepository, battleParticipantRepository);
+    }
+
+    @Test
+    void 배틀참가자를_생성한다() {
+        //given
+        final Long memberId = createMember().getId();
+        final Battle recruitingBattle = createBattleWithStatus(BattleStatus.RECRUITING);
+
+        //when
+        //then
+        assertDoesNotThrow(() -> battleParticipantService.create(recruitingBattle.getId(), memberId));
+    }
+
+    @Test
+    void 배틀참가자가_참가하려는_배틀이_없을_경우_예외를_던진다() {
+        //given
+        final Long memberId = createMember().getId();
+        final long notExistBattleId = -1L;
+
+        //when
+        //then
+        assertThatThrownBy(() -> battleParticipantService.create(notExistBattleId, memberId))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 배틀참가자가_참가하려는_배틀이_꽉_찼을_때_예외를_던진다() {
+        //given
+        final Long memberId = createMember().getId();
+        final Battle battle = createBattleWithParticipantSize(new BattleParticipantSize(1));
+        final Long battleId = battle.getId();
+        battleParticipantRepository.save(BattleParticipant.manager(battleId, memberId));
+
+        //when
+        //then
+        assertThatThrownBy(() -> battleParticipantService.create(battleId, memberId))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "배틀의 상태가 {0}일 때")
+    @CsvSource(value = {"RECRUITING_FINISHED", "PROGRESS", "COMPLETE"})
+    void 배틀참가자가_참가하려는_배틀이_모집중이_아닐_경우_예외를_던진다(final BattleStatus invalidBattleStatus) {
+        //given
+        final Long memberId = createMember().getId();
+        final Battle battleWithStatus = createBattleWithStatus(invalidBattleStatus);
+        final Long battleId = battleWithStatus.getId();
+
+        //when
+        //then
+        assertThatThrownBy(() -> battleParticipantService.create(battleId, memberId))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private Member createMember() {
+        return memberRepository.save(Member.withoutId(new MemberNickname("nickname")));
+    }
+
+    private Battle createBattleWithStatus(final BattleStatus status) {
+        final Battle battle = BattleFixture.initialBattleBuilder()
+            .status(status)
+            .build();
+        return battleRepository.save(battle);
+    }
+
+    private Battle createBattleWithParticipantSize(final BattleParticipantSize participantSize) {
+        final Battle battle = BattleFixture.initialBattleBuilder()
+            .battleParticipantSize(participantSize)
+            .build();
+
+        return battleRepository.save(battle);
+    }
+}
