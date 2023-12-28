ea83f4af006faff87842eb7c6f4573fb4bc040b7
splitCoding <voicesplit@daum.net>
2023-12-28 12:30:48 +0900

    test: 멤버 포인트 생성 기능 테스트 작성

---
+++ src/test/java/com/poolex/poolex/point/controller/MemberPointControllerTest.java
@@ -0,0 +1,49 @@
+package com.poolex.poolex.point.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.poolex.poolex.member.domain.MemberRepository;
+import com.poolex.poolex.point.service.dto.PointCreateRequest;
+import com.poolex.poolex.support.IntegrationTest;
+import com.poolex.poolex.support.ReplaceUnderScoreTest;
+import com.poolex.poolex.support.TestMemberTokenGenerator;
+import com.poolex.poolex.token.JwtTokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+class MemberPointControllerTest extends IntegrationTest implements ReplaceUnderScoreTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    private TestMemberTokenGenerator memberTokenGenerator;
+
+    @BeforeEach
+    void setUp() {
+        this.memberTokenGenerator = new TestMemberTokenGenerator(memberRepository, jwtTokenProvider);
+    }
+
+    @Test
+    void 멤버포인트를_생성한다() throws Exception {
+        //given
+        final PointCreateRequest pointCreateRequest = new PointCreateRequest(10);
+        final String accessToken = memberTokenGenerator.createTokenWithNewMember("nickname");
+        
+        //when
+        //then
+        mockMvc.perform(
+                post("/points")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .content(objectMapper.writeValueAsString(pointCreateRequest))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isCreated());
+    }
+}
---
+++ src/test/java/com/poolex/poolex/point/service/MemberPointServiceTest.java
@@ -0,0 +1,53 @@
+package com.poolex.poolex.point.service;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import com.poolex.poolex.member.domain.Member;
+import com.poolex.poolex.member.domain.MemberNickname;
+import com.poolex.poolex.member.domain.MemberRepository;
+import com.poolex.poolex.point.domain.MemberPoint;
+import com.poolex.poolex.point.domain.MemberPointRepository;
+import com.poolex.poolex.point.service.dto.PointCreateRequest;
+import com.poolex.poolex.support.ReplaceUnderScoreTest;
+import com.poolex.poolex.support.UsingDataJpaTest;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class MemberPointServiceTest extends UsingDataJpaTest implements ReplaceUnderScoreTest {
+
+    @Autowired
+    private MemberPointRepository memberPointRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private MemberPointService memberPointService;
+
+    @BeforeEach
+    void setUp() {
+        this.memberPointService = new MemberPointService(memberPointRepository, memberRepository);
+    }
+
+    @Test
+    void 멤버_포인트를_생성한다() {
+        //given
+        final Member member = memberRepository.save(Member.withoutId(new MemberNickname("nickname")));
+        final PointCreateRequest request = new PointCreateRequest(10);
+
+        //when
+        memberPointService.createPoint(member.getId(), request.getPoint());
+
+        //then
+        final List<MemberPoint> memberPoints = memberPointRepository.findAll();
+        assertSoftly(
+            softly -> {
+                softly.assertThat(memberPoints).hasSize(1);
+                final MemberPoint memberPoint = memberPoints.get(0);
+                softly.assertThat(memberPoint.getPoint().getValue()).isEqualTo(request.getPoint());
+                softly.assertThat(memberPoint.getMemberId()).isEqualTo(member.getId());
+            }
+        );
+    }
+}
