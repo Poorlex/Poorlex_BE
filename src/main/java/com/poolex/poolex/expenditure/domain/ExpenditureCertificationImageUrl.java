3720aea51f59d42e7c0a2755de66b4efe30f9c99
splitCoding <voicesplit@daum.net>
2023-12-30 16:38:36 +0900

    refactor: 지출, 지출이미지 외래키 제약사항 제거

--- src/main/java/com/poolex/poolex/expenditure/domain/ExpenditureCertificationImageUrl.java
+++ src/main/java/com/poolex/poolex/expenditure/domain/ExpenditureCertificationImageUrl.java
@@ -1,7 +1,9 @@
 package com.poolex.poolex.expenditure.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +26,7 @@ public class ExpenditureCertificationImageUrl {
     @Column(name = "url", columnDefinition = "text", nullable = false)
     private String value;
     @ManyToOne
-    @JoinColumn(name = "expenditure_id", nullable = false)
+    @JoinColumn(name = "expenditure_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Expenditure expenditure;
 
     public ExpenditureCertificationImageUrl(final Long id, final String value, final Expenditure expenditure) {
