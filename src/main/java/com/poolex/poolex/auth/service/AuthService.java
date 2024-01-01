d1bc9d14276431d5923da7d27c988787f3a300c8
splitCoding <voicesplit@daum.net>
2024-01-01 17:30:10 +0900

    feat: 가입, 로그인 기능 구현

---
+++ src/main/java/com/poolex/poolex/auth/service/AuthService.java
@@ -0,0 +1,35 @@
+package com.poolex.poolex.auth.service;
+
+import com.poolex.poolex.auth.domain.Member;
+import com.poolex.poolex.auth.domain.MemberNickname;
+import com.poolex.poolex.auth.domain.MemberRepository;
+import com.poolex.poolex.auth.service.dto.request.LoginRequest;
+import com.poolex.poolex.auth.service.dto.response.LoginTokenResponse;
+import com.poolex.poolex.token.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Transactional
+    public LoginTokenResponse loginAfterRegisterIfNotExist(final LoginRequest request) {
+        final Member member = memberRepository.findByOauthId(request.getOauthId())
+            .orElse(createMember(request));
+        final String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+
+        return new LoginTokenResponse(accessToken);
+    }
+
+    private Member createMember(final LoginRequest request) {
+        return memberRepository.save(
+            Member.withoutId(request.getOauthId(), new MemberNickname(request.getNickname()))
+        );
+    }
+}
