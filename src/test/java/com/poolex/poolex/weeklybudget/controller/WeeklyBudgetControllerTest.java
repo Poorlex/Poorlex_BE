4a9db214cbeb0aba695dc877feeaaa506d796828
splitCoding <voicesplit@daum.net>
2024-01-03 01:57:28 +0900

    test: 멤버의 주간 예산 조회 기능 테스트 작성

--- src/test/java/com/poolex/poolex/weeklybudget/controller/WeeklyBudgetControllerTest.java
+++ src/test/java/com/poolex/poolex/weeklybudget/controller/WeeklyBudgetControllerTest.java
@@ -1,15 +1,25 @@
 package com.poolex.poolex.weeklybudget.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.poolex.poolex.auth.domain.Member;
+import com.poolex.poolex.auth.domain.MemberNickname;
 import com.poolex.poolex.auth.domain.MemberRepository;
 import com.poolex.poolex.support.IntegrationTest;
 import com.poolex.poolex.support.ReplaceUnderScoreTest;
 import com.poolex.poolex.support.TestMemberTokenGenerator;
 import com.poolex.poolex.token.JwtTokenProvider;
+import com.poolex.poolex.weeklybudget.domain.WeeklyBudget;
+import com.poolex.poolex.weeklybudget.domain.WeeklyBudgetAmount;
+import com.poolex.poolex.weeklybudget.domain.WeeklyBudgetDuration;
+import com.poolex.poolex.weeklybudget.domain.WeeklyBudgetRepository;
 import com.poolex.poolex.weeklybudget.service.dto.request.WeeklyBudgetCreateRequest;
+import com.poolex.poolex.weeklybudget.service.dto.request.WeeklyBudgetRequest;
+import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +34,9 @@ class WeeklyBudgetControllerTest extends IntegrationTest implements ReplaceUnder
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    @Autowired
+    private WeeklyBudgetRepository weeklyBudgetRepository;
+
     private TestMemberTokenGenerator memberTokenGenerator;
 
     @BeforeEach
@@ -35,7 +48,7 @@ class WeeklyBudgetControllerTest extends IntegrationTest implements ReplaceUnder
     void 주간_예산을_생성한다() throws Exception {
         //given
         final WeeklyBudgetCreateRequest request = new WeeklyBudgetCreateRequest(10000);
-        final String accessToken = memberTokenGenerator.createTokenWithNewMember("nickname");
+        final String accessToken = memberTokenGenerator.createTokenWithNewMember("oauthId");
 
         //when
         //then
@@ -47,5 +60,62 @@ class WeeklyBudgetControllerTest extends IntegrationTest implements ReplaceUnder
             )
             .andDo(print())
             .andExpect(status().isCreated());
+    }
+
+    @Test
+    void 주간_예산을_조회한다_존재할_때() throws Exception {
+        //given
+        final Member member = createMember("oauthId");
+        final WeeklyBudget weaklyBudget = createWeaklyBudget(member.getId(), 10000);
+        final String accessToken = memberTokenGenerator.createAccessToken(member);
+        final WeeklyBudgetRequest request = new WeeklyBudgetRequest(weaklyBudget.getDuration().getStart());
+
+        //when
+        //then
+        mockMvc.perform(
+                get("/weekly-budgets")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.exist").value(true))
+            .andExpect(jsonPath("$.amount").value(10000));
+    }
+
+    @Test
+    void 주간_예산을_조회한다_존재하지_않을_때() throws Exception {
+        //given
+        final Member member = createMember("oauthId");
+        final String accessToken = memberTokenGenerator.createAccessToken(member);
+        final WeeklyBudgetRequest request = new WeeklyBudgetRequest(LocalDate.now());
+
+        //when
+        //then
+        mockMvc.perform(
+                get("/weekly-budgets")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.exist").value(false))
+            .andExpect(jsonPath("$.amount").value(0));
+    }
+
+    private Member createMember(final String oauthId) {
+        return memberRepository.save(Member.withoutId(oauthId, new MemberNickname("nickname")));
+    }
+
+    private WeeklyBudget createWeaklyBudget(final Long memberId, final int amount) {
+        final WeeklyBudget weeklyBudget = WeeklyBudget.withoutId(
+            new WeeklyBudgetAmount(amount),
+            WeeklyBudgetDuration.current(),
+            memberId
+        );
+
+        return weeklyBudgetRepository.save(weeklyBudget);
     }
 }
--- src/test/java/com/poolex/poolex/weeklybudget/service/WeeklyBudgetServiceTest.java
+++ src/test/java/com/poolex/poolex/weeklybudget/service/WeeklyBudgetServiceTest.java
@@ -1,5 +1,6 @@
 package com.poolex.poolex.weeklybudget.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import com.poolex.poolex.auth.domain.Member;
@@ -8,8 +9,11 @@ import com.poolex.poolex.auth.domain.MemberRepository;
 import com.poolex.poolex.support.ReplaceUnderScoreTest;
 import com.poolex.poolex.support.UsingDataJpaTest;
 import com.poolex.poolex.weeklybudget.domain.WeeklyBudget;
+import com.poolex.poolex.weeklybudget.domain.WeeklyBudgetAmount;
 import com.poolex.poolex.weeklybudget.domain.WeeklyBudgetDuration;
 import com.poolex.poolex.weeklybudget.domain.WeeklyBudgetRepository;
+import com.poolex.poolex.weeklybudget.service.dto.response.WeeklyBudgetResponse;
+import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -51,5 +55,48 @@ class WeeklyBudgetServiceTest extends UsingDataJpaTest implements ReplaceUnderSc
                 softly.assertThat(weeklyBudget.getMemberId()).isEqualTo(member.getId());
             }
         );
+    }
+
+    @Test
+    void 멤버의_현재날짜_기준_주간_예산을_조회한다_존재할때() {
+        //given
+        final Member member = memberRepository.save(Member.withoutId("oauthId", new MemberNickname("nickname")));
+        final WeeklyBudget weaklyBudget = createWeaklyBudget(member.getId(), 10000);
+
+        //when
+        final WeeklyBudgetResponse weeklyBudgetResponse = weeklyBudgetService.findCurrentBudgetByMemberIdAndDate(
+            member.getId(),
+            weaklyBudget.getDuration().getStart()
+        );
+
+        //then
+        assertThat(weeklyBudgetResponse.isExist()).isTrue();
+        assertThat(weeklyBudgetResponse.getAmount()).isEqualTo(weaklyBudget.getAmount());
+    }
+
+    @Test
+    void 멤버의_현재날짜_기준_주간_예산을_조회한다_존재하지_않을_때() {
+        //given
+        final Member member = memberRepository.save(Member.withoutId("oauthId", new MemberNickname("nickname")));
+
+        //when
+        final WeeklyBudgetResponse weeklyBudgetResponse = weeklyBudgetService.findCurrentBudgetByMemberIdAndDate(
+            member.getId(),
+            LocalDate.now()
+        );
+
+        //then
+        assertThat(weeklyBudgetResponse.isExist()).isFalse();
+        assertThat(weeklyBudgetResponse.getAmount()).isZero();
+    }
+
+    private WeeklyBudget createWeaklyBudget(final Long memberId, final int amount) {
+        final WeeklyBudget weeklyBudget = WeeklyBudget.withoutId(
+            new WeeklyBudgetAmount(amount),
+            WeeklyBudgetDuration.current(),
+            memberId
+        );
+
+        return weeklyBudgetRepository.save(weeklyBudget);
     }
 }
