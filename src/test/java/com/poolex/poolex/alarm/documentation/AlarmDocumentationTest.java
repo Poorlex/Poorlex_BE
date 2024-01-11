6be104906a6e7711981d167fadb23f436380028f
splitCoding <voicesplit@daum.net>
2024-01-11 17:03:41 +0900

    test: 배틀 알림 조회, 미확인 배틀 알림 개수 조회 문서화 테스트 작성, adoc 파일 수정 및 작성

--- src/test/java/com/poolex/poolex/alarm/documentation/AlarmDocumentationTest.java
+++ src/test/java/com/poolex/poolex/alarm/documentation/AlarmDocumentationTest.java
@@ -7,13 +7,16 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.poolex.poolex.alarm.controller.AlarmController;
 import com.poolex.poolex.alarm.service.AlarmService;
-import com.poolex.poolex.alarm.service.dto.request.BattleAlarmResponse;
+import com.poolex.poolex.alarm.service.dto.request.BattleAlarmRequest;
+import com.poolex.poolex.alarm.service.dto.response.BattleAlarmResponse;
+import com.poolex.poolex.alarm.service.dto.response.UncheckedBattleAlarmCountResponse;
 import com.poolex.poolex.support.RestDocsDocumentationTest;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,6 +24,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -39,7 +44,8 @@ class AlarmDocumentationTest extends RestDocsDocumentationTest {
         //given
         mockingTokenInterceptor();
         mockingMemberArgumentResolver();
-        given(alarmService.findBattleAlarms(any())).willReturn(
+        final BattleAlarmRequest request = new BattleAlarmRequest(LocalDateTime.now());
+        given(alarmService.findBattleAlarms(any(), any(), any())).willReturn(
             List.of(
                 new BattleAlarmResponse(1L, 1L, "EXPENDITURE_CREATED", LocalDateTime.now()),
                 new BattleAlarmResponse(1L, 1L, "BATTLE_NOTIFICATION_CHANGED", LocalDateTime.now())
@@ -47,7 +53,12 @@ class AlarmDocumentationTest extends RestDocsDocumentationTest {
         );
 
         //when
-        final ResultActions result = mockMvc.perform(get("/battles/1/alarms"));
+        final ResultActions result = mockMvc.perform(
+            get("/battles/1/alarms")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer {accessToken}")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+        );
 
         //then
         result.andExpect(status().isOk())
@@ -55,6 +66,9 @@ class AlarmDocumentationTest extends RestDocsDocumentationTest {
                 document("battle-alarm-find",
                     preprocessRequest(prettyPrint()),
                     preprocessResponse(prettyPrint()),
+                    requestFields(
+                        fieldWithPath("dateTime").type(JsonFieldType.STRING).description("요청 시간 [ yyyy-mm-ddThh:mm ]")
+                    ),
                     responseFields()
                         .andWithPrefix("[].",
                             fieldWithPath("alarmId").type(JsonFieldType.NUMBER).description("알림 Id"),
@@ -63,6 +77,32 @@ class AlarmDocumentationTest extends RestDocsDocumentationTest {
                             fieldWithPath("createdAt").type(JsonFieldType.STRING)
                                 .description("알림 생성 시간 [ yyyy-mm-ddThh:mm ]")
                         )
+                ));
+    }
+
+    @Test
+    void find_unchecked_battle_alarm_count() throws Exception {
+        //given
+        mockingTokenInterceptor();
+        mockingMemberArgumentResolver();
+        given(alarmService.getBattleParticipantUncheckedAlarmCount(any(), any()))
+            .willReturn(new UncheckedBattleAlarmCountResponse(1));
+
+        //when
+        final ResultActions result = mockMvc.perform(
+            get("/battles/1/alarms/unchecked")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer {accessToken}")
+        );
+
+        //then
+        result.andExpect(status().isOk())
+            .andDo(
+                document("battle-unchecked-alarm-count",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    responseFields(
+                        fieldWithPath("count").type(JsonFieldType.NUMBER).description("회원이 읽지 않은 배틀 알림 수")
+                    )
                 ));
     }
 }
