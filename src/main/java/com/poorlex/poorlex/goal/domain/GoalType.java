c7cc98842445ecce8910217f751ced1cc5f3aa9a
splitCoding <voicesplit@daum.net>
2024-01-21 04:11:39 +0900

    feat: GoalType 의 추천 목표명 추가

--- src/main/java/com/poorlex/poorlex/goal/domain/GoalType.java
+++ src/main/java/com/poorlex/poorlex/goal/domain/GoalType.java
@@ -5,19 +5,49 @@ import java.util.List;
 import java.util.Optional;
 
 public enum GoalType {
-    STABLE_FUTURE(List.of()),
-    WEALTH_AND_HONOR(List.of()),
-    STRESS_RESOLVE(List.of()),
-    SUCCESSFUL_AT_WORK(List.of()),
-    REST_AND_REFRESH(List.of());
-    private final List<GoalName> recommends;
+    STABLE_FUTURE(List.of(
+        new GoalName("목돈 마련"),
+        new GoalName("대출 상환"),
+        new GoalName("내 집 마련"),
+        new GoalName("비상금 모으기"),
+        new GoalName("1억 모으기"),
+        new GoalName("결혼 자금"),
+        new GoalName("노후 준비")
+    )),
+    WEALTH_AND_HONOR(List.of(
+        new GoalName("목돈 마련"),
+        new GoalName("대출 상환"),
+        new GoalName("내 집 마련"),
+        new GoalName("비상금 모으기"),
+        new GoalName("1억 모으기"),
+        new GoalName("결혼 자금"),
+        new GoalName("노후 준비")
+    )),
+    STRESS_RESOLVE(List.of(
+        new GoalName("근사한 식사"),
+        new GoalName("나에게 어울리는 옷 장만"),
+        new GoalName("고생한 나를 위한 용돈"),
+        new GoalName("취미 생활비")
+    )),
+    SUCCESSFUL_AT_WORK(List.of(
+        new GoalName("제 2외국어 배우기"),
+        new GoalName("스터디 클래스"),
+        new GoalName("자격증 취득하기")
+    )),
+    REST_AND_REFRESH(List.of(
+        new GoalName("여행 자금 모으기"),
+        new GoalName("휴가비"),
+        new GoalName("고생한 나를 위한 용돈"),
+        new GoalName("퇴사 비상자금")
+    ));
+    private final List<GoalName> recommendNames;
 
-    GoalType(final List<GoalName> recommends) {
-        this.recommends = recommends;
+    GoalType(final List<GoalName> recommendNames) {
+        this.recommendNames = recommendNames;
     }
 
-    public List<GoalName> getRecommends() {
-        return recommends;
+    public List<GoalName> getRecommendNames() {
+        return recommendNames;
     }
 
     public static Optional<GoalType> findByName(final String name) {
