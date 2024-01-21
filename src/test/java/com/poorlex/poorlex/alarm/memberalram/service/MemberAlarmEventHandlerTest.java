e54738a93015b8b18f8e0cb4ee350085fb1eadb6
splitCoding <voicesplit@daum.net>
2024-01-22 08:56:25 +0900

    test: 친구 요청 수락, 거절 시 친구 요청 알림 타입 업데이트 기능 테스트 추가

--- src/test/java/com/poorlex/poorlex/alarm/memberalram/service/MemberAlarmEventHandlerTest.java
+++ src/test/java/com/poorlex/poorlex/alarm/memberalram/service/MemberAlarmEventHandlerTest.java
@@ -8,6 +8,9 @@ import com.poorlex.poorlex.alarm.memberalram.domain.MemberAlarmType;
 import com.poorlex.poorlex.friend.service.event.FriendAcceptedEvent;
 import com.poorlex.poorlex.friend.service.event.FriendDeniedEvent;
 import com.poorlex.poorlex.friend.service.event.FriendInvitedEvent;
+import com.poorlex.poorlex.member.domain.Member;
+import com.poorlex.poorlex.member.domain.MemberNickname;
+import com.poorlex.poorlex.member.domain.MemberRepository;
 import com.poorlex.poorlex.support.ReplaceUnderScoreTest;
 import com.poorlex.poorlex.support.UsingDataJpaTest;
 import java.util.List;
@@ -19,6 +22,10 @@ class MemberAlarmEventHandlerTest extends UsingDataJpaTest implements ReplaceUnd
 
     @Autowired
     private MemberAlarmRepository memberAlarmRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
     private MemberAlarmEventHandler memberAlarmEventHandler;
 
     @BeforeEach
@@ -27,7 +34,7 @@ class MemberAlarmEventHandlerTest extends UsingDataJpaTest implements ReplaceUnd
     }
 
     @Test
-    void 친구_초대_이벤트_발생시_친구_초대_알림을_생성한다() {
+    void 친구_초대_이벤트_발생시_초대받은_멤버의_친구_초대_알림을_생성한다() {
         //given
         final FriendInvitedEvent friendInvitedEvent = FriendInvitedEvent.builder()
             .inviteMemberId(1L)
@@ -47,43 +54,61 @@ class MemberAlarmEventHandlerTest extends UsingDataJpaTest implements ReplaceUnd
                 final MemberAlarm memberAlarm = memberAlarms.get(0);
                 softly.assertThat(memberAlarm.getMemberId()).isEqualTo(friendInvitedEvent.getInvitedMemberId());
                 softly.assertThat(memberAlarm.getTargetId()).isEqualTo(friendInvitedEvent.getInviteMemberId());
-                softly.assertThat(memberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_INVITATION);
+                softly.assertThat(memberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_INVITATION_NOT_ACCEPTED);
             }
         );
     }
 
     @Test
-    void 친구_수락_이벤트_발생시_친구_수락_알림을_생성한다() {
+    void 친구_수락_이벤트_발생시_초대한_멤버에게_친구_수락_알림을_생성하고_초대를_받은_멤버의_알림을_수락함으로_변경한다() {
         //given
+        final Member inviteMember = createMemberWithOauthId("oauthId1", "invitor");
+        final Member invitedMember = createMemberWithOauthId("oauthId2", "invited");
+
+        createFriendInviteAlarm(inviteMember, invitedMember);
+
         final FriendAcceptedEvent friendAcceptedEvent = FriendAcceptedEvent.builder()
-            .inviteMemberId(1L)
-            .acceptMemberId(2L)
+            .inviteMemberId(inviteMember.getId())
+            .acceptMemberId(invitedMember.getId())
             .build();
 
         //when
         memberAlarmEventHandler.friendInvitationAccepted(friendAcceptedEvent);
 
         //then
-        final List<MemberAlarm> memberAlarms = memberAlarmRepository.findAll();
+        final Long inviteMemberId = inviteMember.getId();
+        final Long invitedMemberId = invitedMember.getId();
+        final List<MemberAlarm> invitedMemberAlarms = memberAlarmRepository.findAllByMemberId(invitedMemberId);
+        final List<MemberAlarm> inviteMemberAlarms = memberAlarmRepository.findAllByMemberId(inviteMemberId);
 
         assertSoftly(
             softly -> {
-                softly.assertThat(memberAlarms).hasSize(1);
+                softly.assertThat(inviteMemberAlarms).hasSize(1);
+                final MemberAlarm inviteMemberAlarm = inviteMemberAlarms.get(0);
+                softly.assertThat(inviteMemberAlarm.getMemberId()).isEqualTo(inviteMemberId);
+                softly.assertThat(inviteMemberAlarm.getTargetId()).isEqualTo(invitedMemberId);
+                softly.assertThat(inviteMemberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_ACCEPTED);
 
-                final MemberAlarm memberAlarm = memberAlarms.get(0);
-                softly.assertThat(memberAlarm.getMemberId()).isEqualTo(friendAcceptedEvent.getInviteMemberId());
-                softly.assertThat(memberAlarm.getTargetId()).isEqualTo(friendAcceptedEvent.getAcceptMemberId());
-                softly.assertThat(memberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_ACCEPTED);
+                softly.assertThat(invitedMemberAlarms).hasSize(1);
+                final MemberAlarm invitedMemberAlarm = invitedMemberAlarms.get(0);
+                softly.assertThat(invitedMemberAlarm.getMemberId()).isEqualTo(invitedMemberId);
+                softly.assertThat(invitedMemberAlarm.getTargetId()).isEqualTo(inviteMemberId);
+                softly.assertThat(invitedMemberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_INVITATION_ACCEPTED);
             }
         );
     }
 
     @Test
-    void 친구_거절_이벤트_발생시_친구_거절_알림을_생성한다() {
+    void 친구_거절_이벤트_발생시_초대받은_멤버의_알림을_거절로_변경한다() {
         //given
+        final Member inviteMember = createMemberWithOauthId("oauthId1", "invitor");
+        final Member invitedMember = createMemberWithOauthId("oauthId2", "invited");
+
+        createFriendInviteAlarm(inviteMember, invitedMember);
+
         final FriendDeniedEvent friendDeniedEvent = FriendDeniedEvent.builder()
-            .inviteMemberId(1L)
-            .denyMemberId(2L)
+            .inviteMemberId(inviteMember.getId())
+            .denyMemberId(invitedMember.getId())
             .build();
 
         //when
@@ -97,10 +122,23 @@ class MemberAlarmEventHandlerTest extends UsingDataJpaTest implements ReplaceUnd
                 softly.assertThat(memberAlarms).hasSize(1);
 
                 final MemberAlarm memberAlarm = memberAlarms.get(0);
-                softly.assertThat(memberAlarm.getMemberId()).isEqualTo(friendDeniedEvent.getInviteMemberId());
-                softly.assertThat(memberAlarm.getTargetId()).isEqualTo(friendDeniedEvent.getDenyMemberId());
-                softly.assertThat(memberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_DENIED);
+                softly.assertThat(memberAlarm.getMemberId()).isEqualTo(invitedMember.getId());
+                softly.assertThat(memberAlarm.getTargetId()).isEqualTo(inviteMember.getId());
+                softly.assertThat(memberAlarm.getType()).isEqualTo(MemberAlarmType.FRIEND_INVITATION_DENIED);
             }
+        );
+    }
+
+    private Member createMemberWithOauthId(final String oauthId, final String nickname) {
+        final Member member = Member.withoutId(oauthId, new MemberNickname(nickname));
+        return memberRepository.save(member);
+    }
+
+    private void createFriendInviteAlarm(final Member inviteMember, final Member invitedMember) {
+        memberAlarmRepository.save(MemberAlarm.withoutId(
+            invitedMember.getId(),
+            inviteMember.getId(),
+            MemberAlarmType.FRIEND_INVITATION_NOT_ACCEPTED)
         );
     }
 }
