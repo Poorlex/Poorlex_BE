b10e019aa33d7771a6b3139dc2f73cf251595095
splitCoding <voicesplit@daum.net>
2024-02-02 10:08:00 +0900

    test: 배틀 모든 알림 조회 기능 테스트 수정

--- src/test/java/com/poorlex/poorlex/alarm/battlealarm/service/BattleAlarmServiceTest.java
+++ src/test/java/com/poorlex/poorlex/alarm/battlealarm/service/BattleAlarmServiceTest.java
@@ -13,6 +13,12 @@ import com.poorlex.poorlex.battle.domain.Battle;
 import com.poorlex.poorlex.battle.domain.BattleRepository;
 import com.poorlex.poorlex.battle.domain.BattleStatus;
 import com.poorlex.poorlex.battle.fixture.BattleFixture;
+import com.poorlex.poorlex.battlealarmreaction.domain.AlarmReaction;
+import com.poorlex.poorlex.battlealarmreaction.domain.AlarmReactionContent;
+import com.poorlex.poorlex.battlealarmreaction.domain.AlarmReactionRepository;
+import com.poorlex.poorlex.battlealarmreaction.service.AlarmReactionService;
+import com.poorlex.poorlex.battlealarmreaction.service.dto.response.AlarmReactionResponse;
+import com.poorlex.poorlex.common.AbstractCreatedAtResponse;
 import com.poorlex.poorlex.member.domain.Member;
 import com.poorlex.poorlex.member.domain.MemberNickname;
 import com.poorlex.poorlex.member.domain.MemberRepository;
@@ -20,17 +26,39 @@ import com.poorlex.poorlex.participate.domain.BattleParticipant;
 import com.poorlex.poorlex.participate.domain.BattleParticipantRepository;
 import com.poorlex.poorlex.support.ReplaceUnderScoreTest;
 import com.poorlex.poorlex.support.UsingDataJpaTest;
+import com.poorlex.poorlex.voting.vote.domain.Vote;
+import com.poorlex.poorlex.voting.vote.domain.VoteAmount;
+import com.poorlex.poorlex.voting.vote.domain.VoteDuration;
+import com.poorlex.poorlex.voting.vote.domain.VoteDurationType;
+import com.poorlex.poorlex.voting.vote.domain.VoteName;
+import com.poorlex.poorlex.voting.vote.domain.VoteRepository;
+import com.poorlex.poorlex.voting.vote.domain.VoteStatus;
+import com.poorlex.poorlex.voting.vote.service.VoteService;
+import com.poorlex.poorlex.voting.vote.service.dto.response.VoteResponse;
+import com.poorlex.poorlex.voting.votingpaper.domain.VotingPaperRepository;
+import com.poorlex.poorlex.voting.votingpaper.service.VotingPaperService;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-class BattleBattleAlarmServiceTest extends UsingDataJpaTest implements ReplaceUnderScoreTest {
+class BattleAlarmServiceTest extends UsingDataJpaTest implements ReplaceUnderScoreTest {
 
     @Autowired
     private BattleAlarmRepository battleAlarmRepository;
+
+    @Autowired
+    private AlarmReactionRepository alarmReactionRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private VotingPaperRepository votingPaperRepository;
 
     @Autowired
     private BattleRepository battleRepository;
@@ -48,7 +76,12 @@ class BattleBattleAlarmServiceTest extends UsingDataJpaTest implements ReplaceUn
 
     @BeforeEach
     void setUp() {
-        this.battleAlarmService = new BattleAlarmService(battleAlarmRepository, battleAlarmViewHistoryRepository);
+        this.battleAlarmService = new BattleAlarmService(
+            battleAlarmRepository,
+            new VoteService(voteRepository),
+            new VotingPaperService(voteRepository, votingPaperRepository),
+            new AlarmReactionService(alarmReactionRepository, battleAlarmRepository),
+            battleAlarmViewHistoryRepository);
     }
 
     @Test
@@ -59,7 +92,7 @@ class BattleBattleAlarmServiceTest extends UsingDataJpaTest implements ReplaceUn
         final BattleAlarmRequest request = new BattleAlarmRequest(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS));
 
         //when
-        final List<BattleAlarmResponse> battleAlarms = battleAlarmService.findBattleAlarms(battleId, memberId, request);
+        final List<Object> battleAlarms = battleAlarmService.findBattleAlarms(battleId, memberId, request);
 
         //then
         assertThat(battleAlarms).isEmpty();
@@ -69,23 +102,38 @@ class BattleBattleAlarmServiceTest extends UsingDataJpaTest implements ReplaceUn
     void 배틀에_포함된_모든_알림을_조회한다_알림이_있을_때() {
         //given
         final long battleId = 1L;
-        final long memberId = 1L;
+        final Member member = createMemberWithOauthId("oauthId");
         final BattleAlarmRequest request = new BattleAlarmRequest(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS));
-        final BattleAlarm battleAlarm1 = createAlarm(battleId, memberId, BattleAlarmType.EXPENDITURE_CREATED);
-        final BattleAlarm battleAlarm2 = createAlarm(battleId, memberId, BattleAlarmType.EXPENDITURE_NEEDED);
-        final BattleAlarm battleAlarm3 = createAlarm(battleId, memberId, BattleAlarmType.OVER_BUDGET);
+        final BattleAlarm battleAlarm1 = createAlarm(battleId, member.getId(), BattleAlarmType.EXPENDITURE_CREATED);
+        final BattleAlarm battleAlarm2 = createAlarm(battleId, member.getId(), BattleAlarmType.EXPENDITURE_NEEDED);
+        final BattleAlarm battleAlarm3 = createAlarm(battleId, member.getId(), BattleAlarmType.OVER_BUDGET);
+        final AlarmReaction alarmReaction = createAlarmReaction(battleAlarm1.getId(), member.getId());
+        final Vote vote1 = createVote(battleId, member.getId());
+        final Vote vote2 = createVote(battleId, member.getId());
 
         //when
-        final List<BattleAlarmResponse> battleAlarms = battleAlarmService.findBattleAlarms(battleId, memberId, request);
+        final List<Object> battleAlarms = battleAlarmService.findBattleAlarms(battleId, member.getId(), request);
 
         //then
-        final List<BattleAlarmResponse> expectedReponse = List.of(
+        final List<Object> expectedResponse = new ArrayList<>(List.of(
             BattleAlarmResponse.from(battleAlarm1),
             BattleAlarmResponse.from(battleAlarm2),
-            BattleAlarmResponse.from(battleAlarm3)
-        );
-        assertThat(battleAlarms).hasSize(3);
-        assertThat(battleAlarms).usingRecursiveComparison().isEqualTo(expectedReponse);
+            BattleAlarmResponse.from(battleAlarm3),
+            AlarmReactionResponse.from(alarmReaction),
+            new VoteResponse(
+                1L,
+                member.getNickname(),
+                vote1.getName(), vote1.getStatus().name(), vote1.getAmount(), 0, 0, vote1.getStart()
+            ),
+            new VoteResponse(
+                2L,
+                member.getNickname(),
+                vote2.getName(), vote2.getStatus().name(), vote2.getAmount(), 0, 0, vote2.getStart()
+            )
+        ));
+        expectedResponse.sort(Comparator.comparing(o1 -> ((AbstractCreatedAtResponse) o1).getCreatedAt()));
+
+        assertThat(battleAlarms).usingRecursiveComparison().isEqualTo(expectedResponse);
     }
 
     @Test
@@ -127,9 +175,28 @@ class BattleBattleAlarmServiceTest extends UsingDataJpaTest implements ReplaceUn
         return battleRepository.save(BattleFixture.initialBattleBuilder().status(BattleStatus.PROGRESS).build());
     }
 
+    private AlarmReaction createAlarmReaction(final Long alarmId, final Long memberId) {
+        return alarmReactionRepository.save(
+            AlarmReaction.praiseWithoutId(alarmId, memberId, new AlarmReactionContent("칭찬"))
+        );
+    }
+
     private Member createMemberWithOauthId(final String oauthId) {
         final Member member = Member.withoutId(oauthId, new MemberNickname("nickname"));
         return memberRepository.save(member);
+    }
+
+    private Vote createVote(final Long battleId, final Long memberId) {
+        return voteRepository.save(
+            Vote.withoutId(
+                battleId,
+                memberId,
+                new VoteAmount(1000),
+                new VoteDuration(LocalDateTime.now(), VoteDurationType.TEN_MINUTE),
+                new VoteName("초코우유"),
+                VoteStatus.PROGRESS
+            )
+        );
     }
 
     private Member join(final Member member, final Battle battle) {
