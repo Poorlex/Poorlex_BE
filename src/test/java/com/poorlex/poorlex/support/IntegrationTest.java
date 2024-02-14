bf92821ff62b0226dcf64595174e2331de4214a1
splitCoding <voicesplit@daum.net>
2024-02-15 03:28:13 +0900

    refactor: IntegrationTest 변경

--- src/test/java/com/poorlex/poorlex/support/IntegrationTest.java
+++ src/test/java/com/poorlex/poorlex/support/IntegrationTest.java
@@ -1,22 +1,21 @@
 package com.poorlex.poorlex.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poorlex.poorlex.support.db.DataCleanerExtension;
+import com.poorlex.poorlex.support.security.MockUserSecurityTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ActiveProfiles("test")
-@AutoConfigureMockMvc
-@ExtendWith(DataCleanerExtension.class)
 @SpringBootTest
-public abstract class IntegrationTest {
+@ExtendWith(DataCleanerExtension.class)
+public abstract class IntegrationTest implements MockUserSecurityTest {
 
     @Autowired
     protected MockMvc mockMvc;
 
     @Autowired
     protected ObjectMapper objectMapper;
+
 }
