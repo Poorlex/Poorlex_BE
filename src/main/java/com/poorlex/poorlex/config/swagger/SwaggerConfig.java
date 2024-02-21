6200dbb116bbb96e1a0f426284704b98c2843349
splitCoding <voicesplit@daum.net>
2024-02-22 01:25:28 +0900

    feat: Swagger request 서버 재지정

--- src/main/java/com/poorlex/poorlex/config/swagger/SwaggerConfig.java
+++ src/main/java/com/poorlex/poorlex/config/swagger/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +15,16 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        final Server server = new Server()
+            .url("http://poorlex.com")
+            .description("for real API call");
+
+        final Server local = new Server()
+            .url("http://localhost:8080")
+            .description("for real API call");
+
         return new OpenAPI()
+            .servers(List.of(server, local))
             .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
             .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
             .info(new Info().title("Poorlex REST API").description("푸얼렉스 API 문서"));
