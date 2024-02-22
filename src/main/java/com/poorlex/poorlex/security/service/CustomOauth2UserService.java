fa9c8968666f8238860ea05e4b76219fac10eeb3
splitCoding <voicesplit@daum.net>
2024-02-22 17:23:37 +0900

    feat: CustomOauth2UserService 에 애플 로그인 관련 코드 작성

--- src/main/java/com/poorlex/poorlex/security/service/CustomOauth2UserService.java
+++ src/main/java/com/poorlex/poorlex/security/service/CustomOauth2UserService.java
@@ -1,6 +1,10 @@
 package com.poorlex.poorlex.security.service;
 
+import com.poorlex.poorlex.token.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -11,17 +15,51 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private static final String USER_ROLE = "ROLE_USER";
+    private static final String APPLE_REGISTRATION_ID = "apple";
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+
+    public CustomOauth2UserService(final JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Override
     public OAuth2User loadUser(final OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        final DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
-        final OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
         final String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
+        if (registrationId.equalsIgnoreCase(APPLE_REGISTRATION_ID)) {
+            return appleLoginUserProfile(userRequest, registrationId);
+        }
         return new UserProfile(
             registrationId,
             Collections.singleton(new SimpleGrantedAuthority(USER_ROLE)),
-            oAuth2User.getAttributes()
+            defaultOAuth2UserService.loadUser(userRequest).getAttributes()
         );
+    }
+
+    private UserProfile appleLoginUserProfile(final OAuth2UserRequest userRequest, final String registrationId) {
+        final String idToken = userRequest.getAdditionalParameters().get("id_token").toString();
+        final Map<String, Object> attributes = decodeIdTokenPayload(idToken);
+        attributes.put("id_token", idToken);
+        return new UserProfile(
+            registrationId,
+            Collections.singleton(new SimpleGrantedAuthority(USER_ROLE)),
+            attributes
+        );
+    }
+
+    public Map<String, Object> decodeIdTokenPayload(String idToken) {
+        final Claims payload = jwtTokenProvider.getPayload(idToken);
+
+        final Map<String, Object> idTokenClaims = new HashMap<>();
+        idTokenClaims.put("iss", payload.getIssuer());
+        idTokenClaims.put("iat", payload.getIssuedAt());
+        idTokenClaims.put("exp", payload.getExpiration());
+        idTokenClaims.put("aud", payload.getAudience());
+        idTokenClaims.put("sub", payload.getSubject());
+        idTokenClaims.put("email", payload.get("email", String.class));
+
+        return idTokenClaims;
     }
 }
