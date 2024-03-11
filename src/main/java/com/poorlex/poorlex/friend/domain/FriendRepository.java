d00c1458690975e11c5bba94ffa003ae8c25df46
splitCoding <voicesplit@daum.net>
2024-03-11 13:06:22 +0900

    feat: 회원 탈퇴시 친구 삭제 기능 추가

--- src/main/java/com/poorlex/poorlex/friend/domain/FriendRepository.java
+++ src/main/java/com/poorlex/poorlex/friend/domain/FriendRepository.java
@@ -1,8 +1,9 @@
 package com.poorlex.poorlex.friend.domain;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
@@ -13,4 +14,8 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     List<Long> findMembersFriendMemberId(final Long memberId);
 
     boolean existsByFirstMemberIdAndSecondMemberId(final Long firstMemberId, final Long secondMemberId);
+
+    @Query(value = "select f from Friend f " +
+        "where f.firstMemberId = :memberId or f.secondMemberId = :memberId")
+    List<Friend> findFriendsByFirstMemberIdOrSecondMemberId(final Long memberId);
 }
