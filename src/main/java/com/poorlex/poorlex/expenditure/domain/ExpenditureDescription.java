ef573f8adea68842ccec9351b19e9afae0ac96a5
splitCoding <voicesplit@daum.net>
2024-03-24 18:33:51 +0900

    refactor: 지출 설명 에러 메시지 수정

--- src/main/java/com/poorlex/poorlex/expenditure/domain/ExpenditureDescription.java
+++ src/main/java/com/poorlex/poorlex/expenditure/domain/ExpenditureDescription.java
@@ -26,14 +26,16 @@ public class ExpenditureDescription {
         if (!StringUtils.hasText(value)) {
             throw new ApiException(ExceptionTag.EXPENDITURE_DESCRIPTION, "지출 설명이 비어있습니다.");
         }
-        validateLength(value.length());
+        validateLength(value);
     }
 
-    private static void validateLength(final int length) {
+    private static void validateLength(final String value) {
+        final int length = value.length();
         if (MINIMUM_NAME_LENGTH > length || length > MAXIMUM_NAME_LENGTH) {
-            final String errorMessage = String.format("지출 설명은 %d자 이상 %d자 이하여야 합니다. ( 입력 길이 : %d )",
+            final String errorMessage = String.format("지출 설명은 %d자 이상 %d자 이하여야 합니다. ( 입력 설명 : '%s', 입력 길이 : %d )",
                                                       MINIMUM_NAME_LENGTH,
                                                       MAXIMUM_NAME_LENGTH,
+                                                      value,
                                                       length);
             throw new ApiException(ExceptionTag.EXPENDITURE_DESCRIPTION, errorMessage);
         }
