f86efb5ef408478546944be76c2f3f8a1f2afa0c
Kade <23338687+enjay27@users.noreply.github.com>
2024-07-13 13:48:42 +0900

    feat: chatting domain

---
+++ src/main/java/com/poorlex/poorlex/chat/domain/ChattingQueryRepository.java
@@ -0,0 +1,31 @@
+package com.poorlex.poorlex.chat.domain;
+
+import com.poorlex.poorlex.chat.service.dto.response.ChatHistoryResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.poorlex.poorlex.chat.domain.QChatting.chatting;
+
+@Repository
+@RequiredArgsConstructor
+public class ChattingQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public List<ChatHistoryResponse> findByBattleId(Long battleId, Pageable pageable) {
+        return queryFactory.select(Projections.constructor(ChatHistoryResponse.class,
+                            chatting.memberId,
+                            chatting.content,
+                            chatting.type
+                        )).from(chatting)
+                .where(chatting.battleId.eq(battleId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+}
---
+++ src/main/java/com/poorlex/poorlex/chat/domain/MessageType.java
@@ -0,0 +1,7 @@
+package com.poorlex.poorlex.chat.domain;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
